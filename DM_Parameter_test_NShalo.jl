406667f886f1ccf17f1aef549552453a4006992b
jahawort-0 <jahawort@calpoly.edu>
2026-07-08 09:32:06 -0400

    Add files via upload

--- DM_Parameter_test_NShalo.jl
+++ DM_Parameter_test_NShalo.jl
@@ -2,16 +2,20 @@ include("dependencies.jl")
 ##
 # ----- This script is for a NS-centered halo, takes in K and rho_0, returns M_DM
 n = 5
-#Krange = [1e11,2e11,4e11,6e11,8e11, 1e12]
-Krange = [1e9, 1e10, 1e11, 1e12, 1e13, 1e14,1e15]
-#rho_range_0 = [2e14,4e14,6e14,8e14,1e15] #g/cm3    Nuclear density : 2.8e14
-rho_range_0 = [1e9,1e10,1e11,1e12,1e13,1e14,1e15] #g/cm3    Nuclear density : 2.8e14
+#rho_range_0 = [5e13, 6e13, 7e13, 8e13, 9e13, 1e14] #g/cm3  Nuclear density : 2.8e14
+#Krange = [2.5e10, 2.75e10, 3e10, 3.25e10, 3.5e10] 
+rho_range_0 = 10 .^(range(12,16,length = 50))
+Krange = 10 .^(range(9.8,12.3,length = 50))    
+#rho_range_0 = 10 .^ [13.5, 13.6, 13.7, 13.8, 13.9]
+#Krange = 10 .^ [11.1, 11.2, 11.3, 11.4, 11.5]
 rho_range = Math.cgs_density_inv(rho_range_0)
 M_NS1 = 1.4
 R_NS1 = 12
-rend =1000
+rend =2000
 
 M_DMs = zeros(Float64,length(Krange),length(rho_range))
+R_DMs = zeros(Float64,length(Krange),length(rho_range))
+valid_bit = zeros(Bool,length(Krange),length(rho_range))
 #radius_Mh = zeros(Float64,length(Krange),length(rho_range))       #Half mass radius
 profiles = Matrix{Tuple{Vector{Float64},Vector{Float64},Vector{Float64}}}(undef,length(Krange),length(rho_range))
 
@@ -23,23 +27,29 @@ for Ki in range(1,length(Krange))
 
         #radius_Mh[Ki,Mi] = half_mass_interp(M_DMrange[Mi]*0.5)    #half mass radius
         #radii[Ki,Mi] = apply.R_DM
-        rho_bool = BitArray(output[2].>0)
-        lastrho = ((output[2])[rho_bool])[end]
-        ind = findfirst(==(lastrho), output[2])
-        if output[1][ind]<R_NS1
-            M_DMs[Ki,rhoi] = NaN
-        elseif output[2][end]>(0.1/(4*pi*1000^2))
-            M_DMs[Ki,rhoi] = NaN
+        rho_bool = BitArray(output.rho_r.>0)
+        lastrho = ((output.rho_r)[rho_bool])[end]
+        ind = findfirst(==(lastrho), output.rho_r)
+
+        if output.rs[ind]<R_NS1
+            valid_bit[Ki,rhoi] = 0
+        elseif output.R_DM>100
+            valid_bit[Ki,rhoi] = 0
         else
-        M_DMs[Ki,rhoi] = output[4][end]
+            valid_bit[Ki,rhoi] = 1
         end
-        profiles[Ki,rhoi] = ((output[1]), (output[2]), (output[4]))
+        #M_DMs[Ki,rhoi] = output.mass_r[end]
+        M_DMs[Ki,rhoi] = output.M_DM
+        R_DMs[Ki,rhoi] = output.R_DM
+
+        profiles[Ki,rhoi] = ((output.rs), (output.rho_r), (output.mass_r))
     end
 end
 #rho_0s = Math.cgs_density(rho_0s)
 
+## Named Array
 println("")
-println("Enclosed Mass (w/in 1000km) [Msun]")
+println("Enclosed Mass (w/in 2000km) [Msun]")
 radii_named = NamedArray(
     M_DMs,
     (string.(Krange), string.(rho_range_0)),
@@ -47,9 +57,19 @@ radii_named = NamedArray(
 )
 display(radii_named)
 
-# rs, rho_r, mass_r = profiles[2,5]
-# p1 = plot(rs,Math.cgs_density(rho_r), xlabel = "Radius [km]", ylabel = L"Density [g/cm^3]")
-# plot!(xlim=(0,1000))
-# display(p1)
-# p2 = plot(rs,mass_r, xlabel = "Radius [km]", ylabel = "Enclosed Mass [Msun]")
-# display(p2)
+##Heatmap
+M_DM_filter = similar(M_DMs)
+M_DM_filter[valid_bit] .= M_DMs[valid_bit]
+M_DM_filter[.!valid_bit] .= NaN
+phm = heatmap(log10.(rho_range_0),log10.(Krange),M_DM_filter,
+    ylabel = ("log10 K value"), xlabel = "log10 central density [g/cm^3]", 
+    colorbar_title = "enclosed mass", dpi = 500, clim = (0,1)
+    )
+#heatmap(log10.(rho_range_0), log10.(Krange), R_DMs, ylabel = ("log10 K"), xlabel = ("log10 density"), colorbar_title = "DM radius",dpi = 500)
+## plot 1 profile
+rs, rho_r, mass_r = profiles[3,5]
+p1 = plot(rs,Math.cgs_density(rho_r), xlabel = "Radius [km]", ylabel = L"Density [g/cm^3]")
+plot!(p1, xlim=(0,2000))
+display(p1)
+p2 = plot(rs,mass_r, xlabel = "Radius [km]", ylabel = "Enclosed Mass [Msun]",xlim = (0,2000))
+display(p2)
--- playground2.jl
+++ playground2.jl
@@ -4,13 +4,13 @@ include("dependencies.jl")
 n = 5
 K = 1.5e9
 M_DM = 0.1
-M_NS1 = 1.35
-M_NS2 = 1.5
+M_NS1 = 1.3
+M_NS2 = 1.4
 
 #Initialize Polytrope
 poly = Polytrope.compute_polytrope(n)   #integrate nondimensional polytrope
 realpoly = Polytrope.apply_polytrope(poly,M_DM,M_NS1,K,n)   # redimensionalize polytrope
-circ_orbit=Integrate.initial_circular_orbit(M_NS1, M_NS2, M_DM, realpoly);  #Solve for Roche sepeartion, ciruclar orbit
+circ_orbit=Integrate.initial_circular_orbit(M_DM, M_NS1, M_NS2, realpoly);  #Solve for Roche sepeartion, ciruclar orbit
 # initial_circular_orbit returns [a, M_DM, M_NS1, M_NS2, R_DM, J, 0.]
 
 ICs = [circ_orbit[1],circ_orbit[2],circ_orbit[3],circ_orbit[4],0,circ_orbit[6]];    #[a, M_DM, M_NS1, M_NS2, phase, J]
@@ -21,8 +21,9 @@ M1 = M_DM + M_NS1
 η = (M1*M_NS2)/((M1+M_NS2)^2)
 t_decay = 5/256*((Math.c)^5) * (circ_orbit[1]^4) /(Math.G*(M1+M_NS2))^3/ η
 ## Integration
-tend = 1.65729454e5
+tend = 1.94e5
 times=range(0,stop=tend,length=1000)
+#times=range(tend-2.5,stop=tend,length=1000)
 
 # du = zeros(Float64, 6)
 # Integrate.halo_problem!(du,ICs,parameters,0)
@@ -32,6 +33,13 @@ times=range(0,stop=tend,length=1000)
 integration_sol=Integrate.integrate_halo(ICs,parameters,(0,tend)); #The -1 tells the function that we are decreasing in separation
 
 sol_interpolated=integration_sol(times)
+
+# find point of a_min and cut all data after that point
+a_min = 50
+a_diff = sol_interpolated[1,:] .- a_min
+keep = (a_diff.>1e-6)
+sol_interpolated = sol_interpolated[:,keep]
+times = times[keep]
 
 M_DM_t = sol_interpolated[2,:]
 a_t = sol_interpolated[1,:]
@@ -49,4 +57,13 @@ waveform_out=Pipeline.package_quadrupole_frequency_new(full_out)
 df_waveform=DataFrame(waveform_out, ["t", "f", "ddI_p", "ddI_c", "a", "M_DM", "M_NS1", "M_NS2", "Phase"])
 
 ## plot
-plot(df_waveform.Phase,df_waveform.f,xlabel = "phase", ylabel = "frequency")
+
+freq(t,f1) = f1 / ((1-t/t_decay)^(3/8))
+
+p1 = plot(df_waveform.t,df_waveform.f,xlabel = "time [s]", ylabel = "frequency [Hz]", dpi = 200, ylim = (0,60),label = "simulated")
+plot!(p1,df_waveform.t,freq.(df_waveform.t,df_waveform.f[1]), label = "expected", ls=:dash)
+vline!(p1,[t_decay],label = "decay time")
+display(p1)
+# p2 = plot(df_waveform.Phase,df_waveform.ddI_p,xlabel = "phase", ylabel = "ddI", label = "+", dpi = 300)
+# plot!(p2, df_waveform.Phase,df_waveform.ddI_c,xlabel = "phase", ylabel = "ddI", label = "x", dpi = 300)
+#display(p2)
